d08490b55e235283107dd35212892d7a9e57230b
Asadbek <a.nosirjonov@uic.group>
2022-09-12 21:45:25 +0500

    ref: header card

--- src/components/Header-card/index.jsx
+++ src/components/Header-card/index.jsx
@@ -2,16 +2,16 @@ import React from "react";
 import icon from "../../assets/icons/card1.svg";
 import arrow from "../../assets/icons/card-arrow.svg";
 const Card = ({
-  img = icon,
+  img,
   arrowStyle = "block",
   borderSize = "1px",
   titleSize = ".87rem",
-  title = "Select the country in which you want the phone number. Select the site from which to receive SMS.",
-  dashed = "dashed",
+  title = "Как зарегистрировать аккаунт с помощью одноразового номера?",
+  borderStyle,
 }) => {
   return (
     <div
-      className={`py-[1rem] h-[180px] px-[0.75rem] border-${dashed} border-[${borderSize}] rounded-[10px]`}
+      className={`py-[1rem] h-[180px] px-[0.75rem]  border-[${borderSize}] rounded-[10px] border-${borderStyle}`}
     >
       <div className="flex items-center mb-[1.5rem] gap-7">
         <img src={img} alt="icon" />
